b7d18183b6c2dd65e6ca1c1ecc6ae4eb25de7073
Zehan Jurangpathy <jurangpathyz@yahoo.com>
2023-08-29 23:14:41 +0530

    added context class

---
+++ FlexibleData/FlexibleData.Persistance/EntityConfiguration/FlexibleDataEntityTypeConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FlexibleData.Persistance.EntityConfiguration
+{
+    public class FlexibleDataEntityTypeConfiguration : IEntityTypeConfiguration<Domain.Entities.FlexibleData>
+    {
+        public void Configure(EntityTypeBuilder<Domain.Entities.FlexibleData> builder)
+        {
+            builder
+                .ToTable("FlexibleData")
+                .HasKey(t => t.Id);
+        }
+    }
+}
---
+++ FlexibleData/FlexibleData.Persistance/FlexibleDataContext.cs
@@ -0,0 +1,17 @@
+﻿using FlexibleData.Persistance.EntityConfiguration;
+using Microsoft.EntityFrameworkCore;
+
+namespace FlexibleData.Persistance
+{
+    public class FlexibleDataContext : DbContext
+    {
+        public DbSet<Domain.Entities.FlexibleData> FlexibleData { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //register entity type configuration
+            new FlexibleDataEntityTypeConfiguration()
+                .Configure(modelBuilder.Entity<Domain.Entities.FlexibleData>());
+        }
+    }
+}
