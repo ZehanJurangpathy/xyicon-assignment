359be527d857df359fca2542af90a22713c26c7d
Zehan Jurangpathy <jurangpathyz@yahoo.com>
2023-08-29 23:02:23 +0530

    added FlexibleData entity

---
+++ FlexibleData/FlexibleData.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace FlexibleData.Domain.Entities
+{
+    public class BaseEntity
+    {
+        public Guid Id { get; set; }
+    }
+}
---
+++ FlexibleData/FlexibleData.Domain/Entities/FlexibleData.cs
@@ -0,0 +1,7 @@
+﻿namespace FlexibleData.Domain.Entities
+{
+    public class FlexibleData : BaseEntity
+    {
+        public required string Data { get; set; }
+    }
+}
