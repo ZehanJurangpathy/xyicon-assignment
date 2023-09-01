fa9b2e6cd1b838eaf7429930245b7ce7a36b2b35
Zehan Jurangpathy <jurangpathyz@yahoo.com>
2023-09-01 22:36:38 +0530

    Updated statistics processor to calculate unique values

--- FlexibleData/FlexibleData.Application/BackgroundJobs/StatisticsProcessor.cs
+++ FlexibleData/FlexibleData.Application/BackgroundJobs/StatisticsProcessor.cs
@@ -1,6 +1,8 @@
 ﻿using FlexibleData.Application.Contracts.Persistence;
 using FlexibleData.Domain.Entities;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace FlexibleData.Application.BackgroundJobs
 {
@@ -33,19 +35,35 @@ namespace FlexibleData.Application.BackgroundJobs
                     //key already exist. assuming that for one key only one value will be inserted for one API call
                     //hence incrementing the count by one
                     existingKeyDetails.Count += 1;
+                    SetUniqueValues(insertedData, key, existingKeyDetails);
 
                     await _statisticsRepository.UpdateAsync(existingKeyDetails);
                 }
                 else
                 {
                     //key not found. first time the key is inserted to the table
-                    await _statisticsRepository.CreateAsync(new Statistics
+                    var uniqueValues = new HashSet<string>() { insertedData[key] };
+                    var statistics = new Statistics
                     {
                         Key = key,
-                        Count = 1
-                    });
+                        Count = 1,
+                        UniqueCount = JsonConvert.SerializeObject(uniqueValues)
+                    };
+                    SetUniqueValues(insertedData, key, statistics);
+
+                    await _statisticsRepository.CreateAsync(statistics);
                 }
             }
+        }
+
+        private static void SetUniqueValues(Dictionary<string, string> insertedData, string key, Statistics statistics)
+        {
+            //add existing values to a HashSet
+            var uniqueValues = JsonConvert.DeserializeObject<HashSet<string>>(statistics.UniqueCount);
+            //add the new values for the key to the HashSet. value will be added only when the value 
+            //is not present in the HashSet
+            uniqueValues.Add(insertedData[key]);
+            statistics.UniqueCount = JsonConvert.SerializeObject(uniqueValues);
         }
         #endregion
     }
